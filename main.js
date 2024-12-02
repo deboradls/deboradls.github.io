1c5de0fd64a6da0988cd42447adcbffb6a156f10
Débora <deb.limad@gmail.com>
2024-12-02 03:05:54 -0300

    Atualização

--- main.js
+++ main.js
@@ -193,3 +193,11 @@
   });
 
 })()
+
+/*Função para dark tema*/
+function toggleDarkTheme() {
+  document.body.classList.toggle('dark-theme');
+  let toggleDarkThemeButton = document.getElementById("toggle-dark-theme");
+  toggleDarkThemeButton.classList.toggle("bi-sun");
+  toggleDarkThemeButton.classList.toggle("bi-moon");
+}
